5e9665e7a2793c96bf7bc3a34ee636f01f2d1daa
Linortys <Karambos@yandex.ru>
2025-03-18 20:22:50 +0300

    Add files via upload

--- test_check_swag.py
+++ test_check_swag.py
@@ -1,11 +1,21 @@
 from selenium.webdriver.common.by import By
 from conftest import browser
 from pages.swag_labs import SwagLabs
+from pages.base_page import BasePage
 
 
-def test_check_icon(browser):
+def user_name_find(browser):
+    swag_lab = SwagLabs(browser)
+    swag_lab.visit()
+    swag_lab.find_element(locator= 'user-name')
+
+def pass_find(browser):
+    swag_lab = SwagLabs(browser)
+    swag_lab.visit()
+    swag_lab.find_element(locator= 'password')
+
+def test_icon(browser):
     swag_lab = SwagLabs(browser)
     swag_lab.visit()
     assert swag_lab.exist_icon()
-    swag_lab.user_name_find()
-    swag_lab.pass_find()
+
