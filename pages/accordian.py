8e6ad6a6fb0466392bdce6dbc482ebf04acad553
Linortys <Karambos@yandex.ru>
2025-03-31 20:46:47 +0300

    Update accordian.py

--- pages/accordian.py
+++ pages/accordian.py
@@ -7,8 +7,8 @@ class AccordionPage(BasePage):
         self.base_url = 'https://demoqa.com/accordian'
         super().__init__(driver,self.base_url)
 
-        self.sct_element = WebElement(driver, '#section1Content > p')
-        self.hd_element = WebElement(driver, "#section1Heading")
-        self.chk1_element = WebElement(driver, "#section2Content > p:nth-child(1)")
-        self.chk2_element = WebElement(driver, "#section2Content > p:nth-child(2)")
-        self.chk3_element = WebElement(driver, "#section3Content > p")
+        self.element_cont = WebElement(driver, '#section1Content > p')
+        self.element_hd = WebElement(driver, "#section1Heading")
+        self.element_1 = WebElement(driver, "#section2Content > p:nth-child(1)")
+        self.element_2 = WebElement(driver, "#section2Content > p:nth-child(2)")
+        self.element_3 = WebElement(driver, "#section3Content > p")
